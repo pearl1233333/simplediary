871d792e2dea5429e0e869d6a442feec5c29c7a9
pearl <pearl1233333@gamil.com>
2023-08-02 23:22:26 +0900

    커밋

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,13 +1,10 @@
 const DiaryItem = ({ author, content, created_date, emotion, id }) => {
   return (
-    <div className="list_inner">
-      <div className="info">
-        <span className="list_author">작성자 : {author}</span>
-        <span className="list_emotion">감정점수 : {emotion}</span>
-        <br />
-        <span className="date">{new Date(created_date).toLocaleString()}</span>
-      </div>
-      <div className="list_content">{content}</div>
+    <div className="item__inner">
+      <p className="item__author">작성자 : {author}</p>
+      <p className="item__emotion">감정점수 : {emotion}</p>
+      <p className="item__date">{new Date(created_date).toLocaleString()}</p>
+      <p className="item__content">{content}</p>
     </div>
   );
 };
--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -4,9 +4,9 @@ import DiaryItem from "./DiaryItem";
 const DiaryList = ({ diaryList }) => {
   return (
     <div className="DiaryList">
-      <h2>일기 리스트</h2>
-      <p>{diaryList.length}개의 일기가 있습니다.</p>
-      <div className="list">
+      <h2 className="list__title">일기 리스트</h2>
+      <p className="list__total">{diaryList.length}개의 일기가 있습니다.</p>
+      <div className="list__item">
         {diaryList.map((it) => (
           <DiaryItem key={it.id} {...it} />
         ))}
